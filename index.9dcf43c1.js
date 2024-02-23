f86728aa3a86ef17796a46911eba67cc5ae18f1b
Roman Ostrous <ostrous.roman@gmail.com>
2024-02-23 17:59:09 +0200

    make build

---
+++ index.9dcf43c1.js
@@ -0,0 +1,2 @@
+window.addEventListener("hashchange",function(){"#menu"===window.location.hash?document.body.classList.add("page__body--with-menu"):document.body.classList.remove("page__body--with-menu")}),document.getElementById("contact-us-form").addEventListener("submit",function(e){e.preventDefault(),e.target.reset()});
+//# sourceMappingURL=index.9dcf43c1.js.map
